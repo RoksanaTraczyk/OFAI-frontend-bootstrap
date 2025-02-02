8f5eec67cfe23d9e01a359200867f4f1d901386e
RoksanaTraczyk <Roksana.traczyk@wp.pl>
2025-02-02 17:55:54 +0100

    Env and version (#5)
    * added automate deploy when master branch will be changed
    
    * updated worlkflows
    
    * fixed code
    
    * Updated pull-request.yaml
    
    * Removed file
    
    * prepared actions
    
    * fixed letter mistake
    
    * save work
    
    * save work
    
    * save work
    
    * added information about env and version
    
    * save work
    
    * save work
    
    * save work
    
    * save work

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -1,11 +1,15 @@
 /// <reference types="vite/client" />
 
+// Rozszerzenie obiektu import.meta.env przez nową zmienną środowiskową
 interface ImportMetaEnv {
   readonly APP_VERSION: string;
+  readonly VITE_ENVIRONMENT: string;
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
 
+// Zmienna globalna wystawiona przez konfigurację "define"
 declare const __APP_VERSION__: string;
+declare const __VITE_ENVIRONMENT__: string;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import packageJson from './package.json';
+import { resolve } from 'path';
+import { readFileSync } from 'fs';
+
+// Read the version from package.json
+const packageJson = JSON.parse(readFileSync(resolve(__dirname, 'package.json'), 'utf-8'));
+const appVersion = packageJson.version;
 
 export default defineConfig({
   define: {
-    __APP_VERSION__: JSON.stringify(packageJson.version),
+    // __APP_VERSION__: JSON.stringify(packageJson.version),
+    __APP_VERSION__: JSON.stringify(appVersion),
+    __VITE_ENVIRONMENT__ : JSON.stringify(process.env.NODE_ENV),
   },
   server: {
     port: 3000,
